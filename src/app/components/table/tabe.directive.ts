948add281c9211a1e82d502ab1c252724b7219ba
infodusha <infodusha@ya.ru>
2020-03-30 19:50:47 +0300

    Table - fix for quotes

--- src/app/components/table/tabe.directive.ts
+++ src/app/components/table/tabe.directive.ts
@@ -1,4 +1,4 @@
-import {IAugmentedJQuery, IDirective, IDirectiveFactory, ILocationService, IScope, IAttributes} from 'angular'';
+import {IAugmentedJQuery, IDirective, IDirectiveFactory, ILocationService, IScope, IAttributes} from 'angular';
 
 export interface IColumn {
   date: Date,
