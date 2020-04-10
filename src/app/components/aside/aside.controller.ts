1beef1127cbe9aecc34748e0352cc1ad94516cc5
infodusha <infodusha@ya.ru>
2020-04-10 21:49:34 +0300

    Remove unused

--- src/app/components/aside/aside.controller.ts
+++ src/app/components/aside/aside.controller.ts
@@ -1,12 +1,4 @@
-import {asideFields, IAsideScope} from '@components/aside/aside.model';
-import users from '@mocks/user';
-import {preset} from '@app/models/datepicker';
-
 export class AsideController {
-  constructor(private $scope: IAsideScope) {
-    $scope.scope = $scope;
-    $scope.fields = asideFields;
-    $scope.users = users;
-    $scope.datepicker = preset;
+  constructor(private $scope: ng.IScope) {
   }
 }
--- src/app/components/input/input.controller.ts
+++
@@ -1,7 +0,0 @@
-import {IInputScope} from '@components/input/input.model';
-
-export class InputController {
-  constructor(private $scope: IInputScope) {
-    $scope.scope = $scope;
-  }
-}
